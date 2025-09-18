b1e6d33ef39c2aabe57f7c30a1dc5761fe9b7e92
naveenyadav143 <navenrishi32@gamil.com>
2025-09-18 14:50:32 +0530

    Add deployment

--- disaster_platform/settings.py
+++ disaster_platform/settings.py
@@ -57,6 +57,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
